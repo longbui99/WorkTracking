125a138d4ef2e8d3b166cb89b38225e98c8e6b42
builong1999 <philonglml9@gmail.com>
2022-05-26 14:21:22 +0700

    REL

--- jira_migration/models/hr_employee.py
+++ jira_migration/models/hr_employee.py
@@ -4,6 +4,6 @@ from odoo import api, fields, models, _
 class HREmployee(models.Model):
     _inherit = 'hr.employee'
 
-    jira_private_key = fields.Char(string='Access Token')
+    jira_private_key = fields.Char(string='Access Token', tracking=True)
     # jira_migration_board = fields.Many2one('jira.migration', string='JIRA Migration Board')
-    auto_export_work_log = fields.Boolean(string='Auto Export Logs')
+    auto_export_work_log = fields.Boolean(string='Auto Export Logs', tracking=True)
