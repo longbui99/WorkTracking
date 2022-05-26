05a6ffa5e9a9cc1170b376ab19d609ee64794729
builong1999 <philonglml9@gmail.com>
2022-05-26 19:53:21 +0700

    REL

--- project_management/utils/search_parser.py
+++ project_management/utils/search_parser.py
@@ -1,20 +1,18 @@
 
 def get_search_request(keyword):
-    if  isinstance(keyword, list):
-        for key in keyword:
-            if isinstance(keyword, str):
-                if keyword.startswith("jql="):
-                    return "jql", keyword
-                else:
-                    is_project_search = keyword.split("||")
-                    if len(is_project_search) == 2:
-                        return "project_text", is_project_search
-                    else:
-                        is_ticket = keyword.split('-')
-                        try:
-                            int(is_ticket[1])
-                            return "ticket", [keyword]
-                        except Exception:
-                            return "text", keyword
+    if isinstance(keyword, str):
+        if keyword.startswith("jql="):
+            return "jql", keyword
+        else:
+            is_project_search = keyword.split("||")
+            if len(is_project_search) == 2:
+                return "project_text", is_project_search
             else:
-                raise TypeError("You have to provide keyword as a string")
+                is_ticket = keyword.split('-')
+                try:
+                    int(is_ticket[1])
+                    return "ticket", [keyword]
+                except Exception:
+                    return "text", keyword
+    else:
+        raise TypeError("You have to provide keyword as a string")
