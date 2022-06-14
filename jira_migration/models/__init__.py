cceca1b4dc3d64c7d853bc7bd880bed962b3fe71
builong1999 <philonglml9@gmail.com>
2022-06-14 12:20:17 +0700

    REL

--- jira_migration/models/__init__.py
+++ jira_migration/models/__init__.py
@@ -3,5 +3,5 @@ from . import jira_migration
 from . import jira_overall
 from . import jira_ticket
 from . import jira_project
-from . import digest_digest
+# from . import digest_digest
 from . import jira_ac
