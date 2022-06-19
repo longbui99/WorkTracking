664fc9d11947f9485954bb24b299b2e0a8efb737
builong1999 <philonglml9@gmail.com>
2022-06-19 11:43:39 +0700

    REL

--- jira_migration/models/hr_employee.py
+++ jira_migration/models/hr_employee.py
@@ -4,7 +4,8 @@ from odoo import api, fields, models, _
 class HREmployee(models.Model):
     _inherit = 'hr.employee'
 
-    jira_private_key = fields.Char(string='Access Token', tracking=True)
+    jira_private_key = fields.Char(string='Access Token')
+    is_jira_admin = fields.Boolean(string="Admin?", tracking=True)
     auto_export_work_log = fields.Boolean(string='Auto Export Logs', tracking=True)
     auto_remove_access = fields.Boolean(string="Auto Remove Access", default=True)
     maximum_connection = fields.Integer(string="Maximum Extension Connection", default=4)
