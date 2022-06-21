2d41ae85bba1209c0064e5f1ecaa0054567b1bdf
builong1999 <philonglml9@gmail.com>
2022-06-21 11:44:49 +0700

    REL

--- jira_migration/utils/ac_parsing.py
+++ jira_migration/utils/ac_parsing.py
@@ -11,13 +11,14 @@ replace_rule = {
 }
 
 ac_unparsing_rules = [
-    {'pattern': '<b>(.*)</b>', 'value': '**\\1**'},
-    {'pattern': '<b style=".+">(.*)</b>', 'value': '**\\1**'},
-    {'pattern': "</em>", 'value': '*'},
-    {'pattern': "<em.+>", 'value': '*'},
-    {'pattern': '<span style=".+">( )</span>', 'value': ' '},
-    {'pattern': '<span style=".+">(.*)</span>', 'value': '\\1'},
+    {'pattern': '<b>', 'value': '**'},
+    {'pattern': '</b>', 'value': '**'},
+    {'pattern': '</i>', 'value': '*'},
+    {'pattern': "<i>", 'value': '*'},
+    {'pattern': "&nbsp;", 'value': ' '},
+
 ]
+
 
 def parsing(text):
     for rule in replace_rule.keys():
@@ -27,7 +28,7 @@ def parsing(text):
     while index < length:
         for key in ac_rules.keys():
             if index + len(key) <= length:
-                if text[index: index+len(key)] == key:
+                if text[index: index + len(key)] == key:
                     if len(final[final_key]) > 0:
                         if len(key) <= len(final[final_key]):
                             substr = text[pivot:index]
@@ -42,16 +43,27 @@ def parsing(text):
                     else:
                         final.append(text[pivot:index])
                         final.append(key)
-                        final_key = len(final)-1
-                    index += len(key)-1
-                    pivot = index+1
+                        final_key = len(final) - 1
+                    index += len(key) - 1
+                    pivot = index + 1
                     break
-        index += 1 
+        index += 1
     if pivot != index:
         final.append(text[pivot:index])
     return "".join(final)
-    
+
+
 def unparsing(text):
     for syntax in ac_unparsing_rules:
         text = re.sub(syntax['pattern'], syntax['value'], text)
+    index, pivot, length = 0, 0, len(text)
+    while index < length:
+        if text[index] in ("<", '&') :
+            pivot = index
+        elif text[index] in (">", ';') :
+            text = text[:pivot] + text[index+1:]
+            length -= (index-pivot + 1)
+            index -= (index-pivot + 1)
+        index += 1
+
     return text
