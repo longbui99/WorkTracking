aee1ce3e88371b8364db494257b0a578793aec19
builong1999 <philonglml9@gmail.com>
2022-06-22 12:24:12 +0700

    REL

--- jira_migration/models/jira_ac.py
+++ jira_migration/models/jira_ac.py
@@ -19,11 +19,11 @@ class JiraACs(models.Model):
             values['jira_raw_name'] = unparsing(values['name'])
         return super().write(values)
 
-    def update_ac(self, values):
-        res_id = super().update_ac(values)
-        ticket_id = self.browse(res_id).ticket_id
-        if ticket_id.exists():
-            for index, ac in enumerate(ticket_id.ac_ids):
-                ac.sequence = index
-
-        return res_id
+    # def update_ac(self, values):
+    #     res_id = super().update_ac(values)
+    #     ticket_id = self.browse(res_id).ticket_id
+    #     if ticket_id.exists():
+    #         for index, ac in enumerate(ticket_id.ac_ids):
+    #             ac.sequence = index
+    #
+    #     return res_id
