97a4cd2edcd3d32a45a73d7ab6c6d641991dfbae
builong1999 <philonglml9@gmail.com>
2022-07-01 17:24:09 +0700

    REL

--- project_management/utils/time_parsing.py
+++ project_management/utils/time_parsing.py
@@ -3,6 +3,7 @@ from datetime import datetime
 import pytz
 from dateutil.relativedelta import relativedelta
 
+
 def convert_second_to_time_format(time):
     data = [{'key': 'hours', 'duration': 3600},
             {'key': 'minutes', 'duration': 60},
@@ -44,25 +45,28 @@ def convert_log_format_to_second(log_data):
         raise AttributeError("Nothing to log")
     return total_time
 
+
 def get_week_start(self):
     return -(int(self.env['res.lang']._lang_get(self.env.user.lang).week_start) % 7) + 1
+
 
 def get_date_range(self, periodic):
     if periodic[-2:] == "ly":
         periodic = periodic[:-2]
+    tz = pytz.timezone(self.env.user.tz or 'UTC')
     today = datetime.now()
-    start_date, end_date = datetime.now(), datetime.now()
+    start_date, end_date = datetime.now(tz), datetime.now(tz)
     last = 0
     if periodic.startswith("last"):
         last = int(periodic[5])
         periodic = periodic[7:]
     if periodic in ['day', 'dai']:
-        start_date = end_date - relativedelta(days=1+last)
+        start_date = end_date - relativedelta(days=1 + last)
         end_date += relativedelta(days=-last)
     elif periodic == "week":
         week_start = get_week_start(self)
         base_date = today + relativedelta(days=week_start)
-        start_date = base_date - relativedelta(days=base_date.weekday() + week_start + 7*last)
+        start_date = (base_date - relativedelta(days=base_date.weekday() + week_start + 7 * last)).replace(tzinfo=tz)
         end_date = start_date + relativedelta(days=7)
     elif periodic == "month":
         end_date = end_date + relativedelta(months=1 - last, day=1) - relativedelta(days=1)
@@ -71,7 +75,6 @@ def get_date_range(self, periodic):
         current_quarter = today.month // 3
         end_date = today + relativedelta(month=(current_quarter - last) * 3 + 1, day=1) - relativedelta(days=1)
         start_date = end_date - relativedelta(months=2, day=1)
-    tz = pytz.timezone(self.env.user.tz or 'UTC')
-    start_date = (start_date + relativedelta(hour=0, minute=0, second=0)).replace(tzinfo=tz).astimezone(pytz.utc)
-    end_date = (end_date + relativedelta(hour=0, minute=0, second=0)).replace(tzinfo=tz).astimezone(pytz.utc)
+    start_date = (start_date + relativedelta(days=1, hour=0, minute=0, second=0)).astimezone(pytz.utc)
+    end_date = (end_date + relativedelta(days=1, hour=0, minute=0, second=0)).astimezone(pytz.utc)
     return start_date, end_date
