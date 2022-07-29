9cce5ee3b0d63df39f96a2a5a672370b77a2bfc3
longbui-novobi <74339503+longbui-novobi@users.noreply.github.com>
2022-07-29 10:16:33 +0700

    [ADD] Log

--- jira_migration/models/jira_migration.py
+++ jira_migration/models/jira_migration.py
@@ -213,6 +213,7 @@ class JIRAMigration(models.Model):
             jira_key = self.__load_from_key_paths(ticket_fields, issue_mapping.jira_status)
             new_issue_type = self.__load_from_key_paths(ticket_fields, issue_mapping.new_issue_key)
             if ticket.get('key', '-') not in local['dict_ticket_key']:
+                _logger.info(f"Loading for ticket: {ticket['key']}")
                 if not ticket_fields:
                     continue
                 res = {
