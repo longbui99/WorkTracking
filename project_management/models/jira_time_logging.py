7d396cf066fb27a5e13feea4b756694dac3afc69
builong1999 <philonglml9@gmail.com>
2022-08-05 09:52:04 +0700

    REL

--- project_management/models/jira_time_logging.py
+++ project_management/models/jira_time_logging.py
@@ -88,12 +88,12 @@ class JiraTimeLog(models.Model):
     def load_history(self):
         tz = pytz.timezone(self.env.user.tz or 'UTC')
         number_of_days = 1
-        unix = self._context.get('unix')
+        unix = int(self._context.get('unix', 0))
         utc_end_time = (unix and datetime.fromtimestamp(unix) or datetime.now())
         user_end_time = utc_end_time.astimezone(tz) + relativedelta(hour=23, minute=59, second=59)
         end_time = user_end_time.astimezone(pytz.utc)
         if self._context.get('unix_from'):
-            user_start_time = datetime.fromtimestamp(self._context['unix_from']).astimezone(tz) + relativedelta(hour=23, minute=59, second=59)
+            user_start_time = datetime.fromtimestamp(int(self._context['unix_from'])).astimezone(tz) + relativedelta(hour=23, minute=59, second=59)
             start_time = user_start_time.astimezone(pytz.utc)
         else:
             user_start_time = end_time.astimezone(tz) - relativedelta(days=number_of_days, hour=0, minute=0, second=0)
