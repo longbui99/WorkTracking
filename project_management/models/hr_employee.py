6926e9b38d0dbefb2720b69471ee7a3e748424a2
builong1999 <philonglml9@gmail.com>
2022-08-07 09:02:33 +0700

    REL

--- project_management/models/hr_employee.py
+++ project_management/models/hr_employee.py
@@ -7,7 +7,6 @@ class HREmployee(models.Model):
     maximum_search_result = fields.Integer(string="# Search Result", default=11)
     maximum_relative_result = fields.Integer(string="# Relative Active", default=4)
     order_style = fields.Char(string="Order Result")
-<<<<<<< HEAD
     favorite_ticket_ids = fields.Many2many("jira.ticket", string="Favorite Tickets")
     week_start = fields.Selection([('1', 'Monday'),
                                    ('2', 'Tuesday'),
@@ -16,6 +15,3 @@ class HREmployee(models.Model):
                                    ('5', 'Friday'),
                                    ('6', 'Saturday'),
                                    ('7', 'Sunday')], string='First Day of Week', required=True, default='1')
-=======
-    favorite_ticket_ids = fields.Many2many("wt.ticket", string="Favorite Tickets")
->>>>>>> qa
