fd6feff7ee652560bf3ddef49d6fb30ba074d54c
builong1999 <philonglml9@gmail.com>
2022-08-08 15:54:38 +0700

    REL

--- wt_migration/models/wt_project.py
+++ wt_migration/models/wt_project.py
@@ -19,7 +19,7 @@ class JiraProject(models.Model):
         for project in self:
             if project.wt_migration_id not in migration_dict:
                 migration_dict[project.wt_migration_id] = self.env['res.users']
-            if project.allowed_user_ids:
+            if project.allowed_user_ids and not (project.allowed_user_ids & migration_dict[project.wt_migration_id]):
                 migration_dict[project.wt_migration_id] |= project.allowed_user_ids[0]
             user_ids = project.allowed_user_ids.ids
             if len(user_ids) == 0 and project.wt_migration_id:
