e1953d4780d695773721839517fedbff88a2b001
builong1999 <philonglml9@gmail.com>
2022-08-12 15:10:09 +0700

    REL

--- project_management/utils/search_parser.py
+++ project_management/utils/search_parser.py
@@ -15,13 +15,13 @@ def get_search_request(string):
         'chain': ('chain', 0, True),
         'sprint': ('sprint', 0, True),
         'mine': ('mine', 0, True),
-        '<[a-zA-Z0-9]+>': ('project', 1, -1),
+        '\[[a-zA-Z0-9]+\]': ('project', 1, -1),
         '[a-zA-Z]+-[0-9]+': ('issue', 0, True),
         '[A-Z]{2,}': ('project', 0, True),
-        '<[a-zA-Z0-9]+-[a-zA-Z0-9]+>': ('issue', 1, -1),
+        '\[[a-zA-Z0-9]+-[a-zA-Z0-9]+\]': ('issue', 1, -1),
         '>[a-zA-Z0-9@.]+<?': ('name', 1, True),
     }
-    interator = re.finditer('(([a-zA-Z]+-[0-9]+:?)|[A-Z]{2,}:?|<[a-zA-Z0-9-]*>|>[a-zA-Z0-9@.]+<?|[ \.]?(chain|mine|sprint\+?)[ \.]?|jql=)', string)
+    interator = re.finditer('(([a-zA-Z]+-[0-9]+:?)|[A-Z]{2,}:?|\[[a-zA-Z0-9-]*\]|>[a-zA-Z0-9@.]+<?|[ \.]?(chain|mine|sprint\+?)[ \.]?|jql=)', string)
     for_delete = []
     for match in interator:
         action = re.sub(truncate_regex, '', match.group())
