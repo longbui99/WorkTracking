c55ed16bf4d4f09b78a5c8f1059bdfc573d579ad
builong1999 <philonglml9@gmail.com>
2022-08-27 21:32:06 +0700

    REL

--- project_management/models/ir_http.py
+++ project_management/models/ir_http.py
@@ -1,7 +1,7 @@
 import jwt
 import json
 from odoo import api, http, models, exceptions
-from odoo.http import request, content_disposition, Response
+from odoo.http import request
 
 
 class IrHttp(models.AbstractModel):
@@ -9,18 +9,14 @@ class IrHttp(models.AbstractModel):
 
     @classmethod
     def _auth_method_jwt(cls):
-        try:
-            if len(request.params) == 0:
-                request.params = json.loads(request.httprequest.data)
-            if not request.params.get('jwt'):
+        if len(request.params) == 0:
+            request.params = json.loads(request.httprequest.data)
+        if not request.params.get('jwt'):
+            raise exceptions.AccessDenied("The JWT uid is required")
+        else:
+            payload = jwt.decode(request.params['jwt'], request.env.cr.dbname + "longlml", algorithms=["HS256"])
+            if not payload.get('uid'):
                 raise exceptions.AccessDenied("The JWT uid is required")
-            else:
-                payload = jwt.decode(request.params['jwt'], request.env.cr.dbname + "longlml", algorithms=["HS256"])
-                if not payload.get('uid'):
-                    raise exceptions.AccessDenied("The JWT uid is required")
-                if not request.env['user.access.code'].sudo().search_count([('key', '=', payload.get('token', False))]):
-                    raise exceptions.AccessDenied("The JWT is incorrect")
-                
-                request.uid = payload['uid']
-        except Exception as e:
-            return http.Response(str(e), content_type='text/http', status=404)
+            if not request.env['user.access.code'].sudo().search_count([('key', '=', payload.get('token', False))]):
+                raise exceptions.SessionExpiredException("The JWT is incorrect")
+            request.uid = payload['uid']
