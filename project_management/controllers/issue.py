c606988472949d8b525ef987330ad127a6cfe179
builong1999 <philonglml9@gmail.com>
2022-08-29 22:51:01 +0700

    REL

--- project_management/controllers/issue.py
+++ project_management/controllers/issue.py
@@ -65,7 +65,7 @@ class WtIssue(http.Controller):
                 "issueName": log.issue_id.issue_name,
                 "description": log.description,
                 "start_date": log.start_date.isoformat(),
-                'type_url': log.issue_id.issue_type_id.img_url
+                'type_url': log.issue_id
             }
 
     @handling_req_res
@@ -197,6 +197,7 @@ class WtIssue(http.Controller):
         ac_id = request.env['wt.ac'].browse(int(id))
         return ac_id
     
+    @handling_req_res
     @http.route(['/management/issue/work-log/update'], type="http", cors="*", methods=['POST'], auth='jwt', csrf=False)
     def update_done_work_logs(self, **kwargs):
         params = json.loads(request.httprequest.data)
