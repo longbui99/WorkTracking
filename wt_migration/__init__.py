9467d89339678f1ba077386c6aa0c937399e2784
builong1999 <philonglml9@gmail.com>
2022-09-13 23:49:37 +0700

    [REL]

--- wt_migration/__init__.py
+++ wt_migration/__init__.py
@@ -7,7 +7,6 @@ from odoo import SUPERUSER_ID, api
 
 
 def post_init_hook(cr, registry):
-    print("-------------------------------------------------")
     cr.execute("""
         CREATE TABLE IF NOT EXISTS token_cache ( key VARCHAR UNIQUE, value VARCHAR);
         CREATE INDEX IF NOT EXISTS token_cache_index ON token_cache USING HASH (key);
